ad8a0e319b8f4183dd837ed7bb2867df7af227e1
Carolina Vasco <correo_de_carolina@gmail.com>
2026-01-17 21:39:18 -0500

    Modify paypal_main

--- backend/services/payment-service/app/main.py
+++ backend/services/payment-service/app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.paypal import router as paypal_router
+from app.routes.webhooks import router as payments_router  # si este archivo se llama webhooks.py
 
 app = FastAPI(title="Payment Service", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["http://localhost:5173"],  # mejor que "*"
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(paypal_router)
+app.include_router(payments_router)
