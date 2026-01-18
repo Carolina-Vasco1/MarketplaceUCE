9dd5fead6ec4de648c247a46098e50ed650f3de8
Carolina Vasco <correo_de_carolina@gmail.com>
2026-01-17 21:38:39 -0500

    Modify paypal_config

--- backend/services/payment-service/app/core/config.py
+++ backend/services/payment-service/app/core/config.py
@@ -1,17 +1,20 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     ENV: str = "local"
     SERVICE_NAME: str = "payment-service"
 
-    KAFKA_BOOTSTRAP: str = "kafka:9092"
+    KAFKA_BOOTSTRAP: str = "kafka:29092"  # OJO: en tu compose kafka interno usa 29092
 
+    PAYPAL_ENV: str = "sandbox"  # ✅ NUEVO
     PAYPAL_BASE_URL: str = "https://api-m.sandbox.paypal.com"
     PAYPAL_CLIENT_ID: str = ""
     PAYPAL_CLIENT_SECRET: str = ""
-    PAYPAL_WEBHOOK_ID: str = ""  # para verificación de firma en prod
+    PAYPAL_WEBHOOK_ID: str = ""
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
