cf2332e8a519d0bd51704dcc8377c39c95666067
Carolina Vasco <correo_de_carolina@gmail.com>
2026-01-19 13:27:30 -0500

    Modify infra_alb

---
+++ marketplace-turbo/infra/terraform/alb.tf
@@ -0,0 +1,37 @@
+resource "aws_lb" "alb" {
+  name               = "${var.project_name}-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [for s in aws_subnet.public : s.id]
+
+  tags = { Name = "${var.project_name}-alb" }
+}
+
+resource "aws_lb_target_group" "gateway_tg" {
+  name        = "${var.project_name}-gw-tg"
+  port        = var.gateway_container_port
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    path                = "/health"
+    matcher             = "200-399"
+    interval            = 20
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.gateway_tg.arn
+  }
+}
